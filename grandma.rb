a3e3421a98e129659744e03bc13e583299b9839d
Albert LaQue <albertlaque@gmail.com>
2020-02-01 01:00:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
-  else 
+  else if phrase == phrase.upcase 
+    "NO, NOT SINCE 1938!"
     
   end
 end 
